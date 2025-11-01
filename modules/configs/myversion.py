44f15e174c11745f8b5ebfd880a372320f3f355f
yusen <sanmusen_wu@outlook.com>
2025-11-01 22:58:41 +0800

    fire

--- modules/configs/myversion.py
+++ modules/configs/myversion.py
@@ -2,6 +2,10 @@ myversion = "1.10.37"
 mychangelog = """
 # Feat
 
-适配了自动版本打包与发布流程
+适配自动版本打包与发布流程
 Auto pakcage and release action flow
+
+# Fix
+
+修复 [pr2](https://github.com/sanmusen214/BAAH_ci_test/pull/2) 
 """
